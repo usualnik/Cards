39ee67a36deac947ab20f47843db6055986a4741
Nikita <usualnik@gmail.com>
2025-09-16 17:45:47 +0300

    new levels

--- Assets/_Scripts/MainMenu/Loader.cs
+++ Assets/_Scripts/MainMenu/Loader.cs
@@ -25,7 +25,26 @@ public class Loader : MonoBehaviour
         {16 , "Assets/Scenes/Game/Level 16.unity"},
         {17 , "Assets/Scenes/Game/Level 17.unity"},
         {18 , "Assets/Scenes/Game/Level 18.unity"},
-        {19 , "Assets/Scenes/Game/Level 19.unity"}
+        {19 , "Assets/Scenes/Game/Level 19.unity"},
+        {20 , "Assets/Scenes/Game/Level 20.unity"},
+        {21 , "Assets/Scenes/Game/Level 21.unity"},
+        {22 , "Assets/Scenes/Game/Level 22.unity"},
+        {23 , "Assets/Scenes/Game/Level 23.unity"},
+        {24 , "Assets/Scenes/Game/Level 24.unity"},
+        {25 , "Assets/Scenes/Game/Level 25.unity"},
+        {26 , "Assets/Scenes/Game/Level 26.unity"},
+        {27 , "Assets/Scenes/Game/Level 27.unity"},
+        {28 , "Assets/Scenes/Game/Level 28.unity"},
+        {29 , "Assets/Scenes/Game/Level 29.unity"},
+        {30 , "Assets/Scenes/Game/Level 30.unity"},
+        {31 , "Assets/Scenes/Game/Level 31.unity"},
+        {32 , "Assets/Scenes/Game/Level 32.unity"},
+        {33 , "Assets/Scenes/Game/Level 33.unity"},
+        {34 , "Assets/Scenes/Game/Level 34.unity"},
+        {35 , "Assets/Scenes/Game/Level 35.unity"},
+        {36 , "Assets/Scenes/Game/Level 36.unity"},
+        {37 , "Assets/Scenes/Game/Level 37.unity"}      
+
     };
 
     private const int LOADING_SCREEN_WINDOW_INDEX = 5;

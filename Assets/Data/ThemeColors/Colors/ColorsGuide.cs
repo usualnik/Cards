370c5abc32aa91222b18780f6e6563303e066a6f
TommmyGunnn <tommmigunnn@gmail.com>
2025-09-19 16:14:25 +0400

    Applied materials, tuned props

---
+++ Assets/Data/ThemeColors/Colors/ColorsGuide.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ColorsGuide", menuName = "Scriptable Objects/ColorsGuide")]
+public class ColorsGuide : ScriptableObject
+{
+    [Header("Amazon Feathers")]
+        public Color _AF1 = new Color32(0xFD, 0x99, 0xD7, 0xFF);
+        public Color _AF2 = new Color32(0x73, 0x61, 0xC3, 0xFF);
+        public Color _AF3 = new Color32(0x68, 0xC0, 0x71, 0xFF);
+        public Color _AF4 = new Color32(0xC7, 0xF5, 0x00, 0xFF);
+        public Color _AF5 = new Color32(0xFF, 0xEE, 0x00, 0xFF);
+        public Color _AF6 = new Color32(0xC3, 0x45, 0xB0, 0xFF);
+        public Color _AF_Floor = new Color32(0x6D, 0x9E, 0x9E, 0xFF);
+        public Color _AF_MainBright = new Color32(0x38, 0xAB, 0x90, 0xFF);
+        public Color _AF_MainDark = new Color32(0x52, 0x69, 0x6A, 0xFF);
+        public Color _AF_Metal = new Color32(0x5A, 0x50, 0x89, 0xFF);
+        public Color _AF_Rubber = new Color32(0x62, 0xB7, 0xB1, 0xFF);
+        public Color _AF_Secondary = new Color32(0xC0, 0x96, 0xB0, 0xFF);
+
+    [Header("Default")]
+        public Color _D1 = new Color32(0xB2, 0x29, 0x66, 0xFF);
+        public Color _D2 = new Color32(0x16, 0x54, 0xB0, 0xFF);
+        public Color _D3 = new Color32(0x3A, 0xB4, 0x4E, 0xFF);
+        public Color _D4 = new Color32(0xEC, 0xA9, 0x1A, 0xFF);
+        public Color _D5 = new Color32(0xAD, 0x2F, 0xE9, 0xFF);
+        public Color _D6 = new Color32(0x15, 0xA3, 0xA7, 0xFF);
+        public Color _D_Floor = new Color32(0xBD, 0x97, 0x9A, 0xFF);
+        public Color _D_MainBright = new Color32(0x40, 0x7F, 0xDD, 0xFF);
+        public Color _D_MainDark = new Color32(0x16, 0x54, 0xB0, 0xFF);
+        public Color _D_Metal = new Color32(0x8C, 0x3D, 0x60, 0xFF);
+        public Color _D_Rubber = new Color32(0x16, 0xC2, 0xF3, 0xFF);
+        public Color _D_Secondary = new Color32(0xE6, 0xAC, 0x1E, 0xFF);
+
+    [Header("Desert Evening")]
+        public Color _DE1;
+        public Color _DE2;
+        public Color _DE3;
+        public Color _DE4;
+        public Color _DE5;
+        public Color _DE6;
+        public Color _DE_Floor;
+        public Color _DE_MainBright;
+        public Color _DE_MainDark;
+        public Color _DE_Metal;
+        public Color _DE_Rubber;
+        public Color _DE_Secondary;
+
+    [Header("Fuchsia Branches")]
+        public Color _FB1;
+        public Color _FB2;
+        public Color _FB3;
+        public Color _FB4;
+        public Color _FB5;
+        public Color _FB6;
+        public Color _FB_Floor;
+        public Color _FB_MainBright;
+        public Color _FB_MainDark;
+        public Color _FB_Metal;
+        public Color _FB_Rubber;
+        public Color _FB_Secondary;
+
+    [Header("Lilac Dawn")]
+        public Color _LD1;
+        public Color _LD2;
+        public Color _LD3;
+        public Color _LD4;
+        public Color _LD5;
+        public Color _LD6;
+        public Color _LD_Floor;
+        public Color _LD_MainBright;
+        public Color _LD_MainDark;
+        public Color _LD_Metal;
+        public Color _LD_Rubber;
+        public Color _LD_Secondary;
+
+    [Header("Vibrant Coral")]
+        public Color _VC1;
+        public Color _VC2;
+        public Color _VC3;
+        public Color _VC4;
+        public Color _VC5;
+        public Color _VC6;
+        public Color _VC_Floor;
+        public Color _VC_MainBright;
+        public Color _VC_MainDark;
+        public Color _VC_Metal;
+        public Color _VC_Rubber;
+        public Color _VC_Secondary;
+}
